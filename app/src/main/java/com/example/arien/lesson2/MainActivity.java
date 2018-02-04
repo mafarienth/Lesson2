372978604e13ba2456551a395c4ba47e52a0d7cc
mafarienth <mugidya.mafarienth@gmail.com>
2018-02-04 18:54:58 +0700

    push yang ketiga kali

--- app/src/main/java/com/example/arien/lesson2/MainActivity.java
+++ app/src/main/java/com/example/arien/lesson2/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.arien.lesson2;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText_menu, editText_porsi;
+    TextView textView_tempat, textView_menu, textView_porsi, textView_harga;
+    Button bEatbus, bAbnormal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText_menu = (EditText) findViewById(R.id.editTextMenu);
+        editText_porsi = (EditText) findViewById(R.id.editTextPorsi);
+        textView_tempat = (TextView) findViewById(R.id.textViewTempat);
+        textView_menu = (TextView) findViewById(R.id.textViewMenu);
+        textView_porsi = (TextView) findViewById(R.id.textViewPorsi);
+        textView_harga = (TextView) findViewById(R.id.textViewHarga);
+        bEatbus = (Button) findViewById(R.id.buttonEatbus);
+        bAbnormal = (Button) findViewById(R.id.buttonAbnormal);
+    }
+
+    public void eatbus(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        int uang = 65500;
+        String uOsas = String.valueOf(uang);
+        intent.putExtra("uOsas", uOsas);
+
+        String namaTempat = "Eatbus";
+        intent.putExtra("namaTempat", namaTempat);
+
+        String menu = editText_menu.getText().toString();
+        intent.putExtra("menu", menu);
+
+        String porsi = editText_porsi.getText().toString();
+        intent.putExtra("porsi", porsi);
+
+        startActivity(intent);
+
+
+    }
+
+    public void apnormal(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        int uang = 65500;
+        String uOsas = String.valueOf(uang);
+        intent.putExtra("uOsas", uOsas);
+
+        String namaTempat = "Abnormal";
+        intent.putExtra("namaTempat", namaTempat);;
+
+        String menu = editText_menu.getText().toString();
+        intent.putExtra("menu", menu);
+
+        String porsi = editText_porsi.getText().toString();
+        intent.putExtra("porsi", porsi);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/arien/lesson2/SecondActivity.java
+++ app/src/main/java/com/example/arien/lesson2/SecondActivity.java
@@ -1,13 +1,106 @@
 package com.example.arien.lesson2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String tempat = intent.getStringExtra("namaTempat");
+
+        Intent intent2 = getIntent();
+        String menu = intent2.getStringExtra("menu");
+
+        Intent intent3 = getIntent();
+        String porsi = intent3.getStringExtra("porsi");
+        int totalPorsi = Integer.parseInt(porsi);
+
+        Intent intent4 = getIntent();
+        String uangOsas = intent4.getStringExtra("uOsas");
+        int uang = Integer.parseInt(uangOsas);
+
+        if (tempat.contains("Eatbus")) {
+            int harga = 50000;
+            int totalHarga = harga * totalPorsi;
+
+            if (uang >= totalHarga) {
+                TextView textView_tempat = findViewById(R.id.textViewTempat);
+                textView_tempat.setText(tempat);
+
+                TextView textView_menu = findViewById(R.id.textViewMenu);
+                textView_menu.setText(menu);
+
+                TextView textView_porsi = findViewById(R.id.textViewPorsi);
+                textView_porsi.setText(porsi);
+
+                String hasil = String.valueOf(totalHarga);
+                TextView textView_harga = findViewById(R.id.textViewHarga);
+                textView_harga.setText(hasil);
+
+                Toast.makeText(this, "Disini aja makan malamnya", Toast.LENGTH_LONG).show();
+            } else {
+                TextView textView_makan = findViewById(R.id.textViewTempat);
+                textView_makan.setText(tempat);
+
+                TextView textView_menu = findViewById(R.id.textViewMenu);
+                textView_menu.setText(menu);
+
+                TextView textView_porsi = findViewById(R.id.textViewPorsi);
+                textView_porsi.setText(porsi);
+
+                String hasil = String.valueOf(totalHarga);
+                TextView textView_harga = findViewById(R.id.textViewHarga);
+
+                textView_harga.setText(hasil);
+
+                Toast.makeText(this, "Jangan disini makan malamnya, uang kamu tidak cukup", Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            int harga = 30000;
+            int totalHarga = harga * totalPorsi;
+            if (uang >= totalHarga) {
+                TextView textView_makan = findViewById(R.id.textViewTempat);
+                textView_makan.setText(tempat);
+
+                TextView textView_menu = findViewById(R.id.textViewMenu);
+                textView_menu.setText(menu);
+
+                TextView textView_porsi = findViewById(R.id.textViewPorsi);
+                textView_porsi.setText(porsi);
+
+                String hasil = String.valueOf(totalHarga);
+                TextView textView_harga = findViewById(R.id.textViewHarga);
+                textView_harga.setText(hasil);
+
+                Toast.makeText(this, "Disini aja makan malamnya", Toast.LENGTH_LONG).show();
+            } else {
+                TextView textView_makan = findViewById(R.id.textViewTempat);
+                textView_makan.setText(tempat);
+
+                TextView textView_menu = findViewById(R.id.textViewMenu);
+                textView_menu.setText(menu);
+
+                TextView textView_porsi = findViewById(R.id.textViewPorsi);
+                textView_porsi.setText(porsi);
+
+                String hasil = String.valueOf(totalHarga);
+                TextView textView_harga = findViewById(R.id.textViewHarga);
+
+                textView_harga.setText(hasil);
+
+                Toast.makeText(this, "Jangan disini makan malamnya, uang kamu tidak cukup", Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
